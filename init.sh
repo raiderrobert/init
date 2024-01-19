ee239cf54e9c0a86d4eb362a0ba0617ffc0e0090
Robert Roskam <raiderrobert@gmail.com>
2024-01-19 14:15:38 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,1 +1,14 @@
+# accept the license
 sudo xcodebuild -license
+
+# install home brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# make a place for repos
+cd ~
+mkdir repos
+
+
+
+
+
